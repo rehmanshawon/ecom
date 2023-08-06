0cd03df0c4dafd2f03ae5931000760422ae6683b
rehmanshawon <meghnadbadh@gmail.com>
2023-08-07 02:13:32 +0600

    corrected

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
-          <p></p>
+          <p>
             Get started now by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
