e28a5af5d2085d94b209c7b19936d42e9043b845
rehmanshawon <meghnadbadh@gmail.com>
2023-08-08 04:42:04 +0600

    transition to ecom

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+import "@/styles/globals.scss";
+import "@/styles/custom.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
