23ab30fe39119985056a162ed9bdac630f0d8e44
rehmanshawon <meghnadbadh@gmail.com>
2023-08-08 04:37:14 +0600

    ts to js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -17,8 +17,8 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Get started now by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
+            Get started by editing&nbsp;
+            <code className={styles.code}>pages/index.js</code>
           </p>
           <div>
             <a
@@ -26,7 +26,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{" "}
+              By{' '}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -110,5 +110,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  );
+  )
 }
