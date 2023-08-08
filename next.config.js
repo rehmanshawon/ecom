03f7c5c299cda406d7315116518f65b14e1f91ff
rehmanshawon <meghnadbadh@gmail.com>
2023-08-08 08:02:43 +0600

    helo

--- next.config.js
+++ next.config.js
@@ -40,8 +40,8 @@ const nextConfig = {
   publicRuntimeConfig: {
     apiUrl:
       process.env.NODE_ENV === "development"
-        ? "https://ecom.bornaengineering.com:3000/api" // development api
-        : "https://ecom.bornaengineering.com:3000/api", // production api
+        ? "http://ecom.bornaengineering.com:3000/api" // development api
+        : "http://ecom.bornaengineering.com:3000/api", // production api
   },
 };
 
