af9a434f726c9284d076f52382b2eb0949aa4a2e
rehmanshawon <rehman.shawon@apsissolutions.com>
2023-08-12 17:56:52 +0600

    host change again

--- next.config.js
+++ next.config.js
@@ -27,11 +27,11 @@ const nextConfig = {
   },
   serverRuntimeConfig: {
     dbConfig: {
-      host: "be-primary.cbbdsky2ogtw.ap-south-1.rds.amazonaws.com",
-      //host: "127.0.0.1",
+      //host: "be-primary.cbbdsky2ogtw.ap-south-1.rds.amazonaws.com",
+      host: "127.0.0.1",
       port: 3306,
       user: "admin",
-      password: "ApsisINT123",
+      password: "ApsisINT@123",
       database: "borna_db",
     },
     secret:
