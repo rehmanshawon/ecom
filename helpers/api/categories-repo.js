d35eb0fa48d37090bf9e7c214e8c778a5e3192ab
rehmanshawon <rehman.shawon@apsissolutions.com>
2023-08-13 05:36:03 +0600

    cat

--- helpers/api/categories-repo.js
+++ helpers/api/categories-repo.js
@@ -11,12 +11,12 @@ export const categoriesRepo = {
 async function getAll() {
   console.log("here i am");
   return await db.Category.findAll({
-    include: [
-      {
-        model: db.Product,
-        attributes: ["id", "sku", "createdAt"],
-      },
-    ],
+    // include: [
+    //   {
+    //     model: db.Product,
+    //     attributes: ["id", "sku", "createdAt"],
+    //   },
+    // ],
     attributes: ["id", "category_name", "createdAt"],
   });
 }
